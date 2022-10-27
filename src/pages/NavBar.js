90e9bf46282e3f3df5e520c751ee955023685a2e
emon <emondas457@gmail.com>
2022-10-27 15:53:38 +0600

    add toggle

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useContext } from 'react';
-import { FaBookReader } from "react-icons/fa";
+import { FaBookReader, FaToggleOff, FaToggleOn } from "react-icons/fa";
 import { Link, useNavigate } from 'react-router-dom';
 import { userContext } from '../context/AuthProvider';
 const NavBar = () => {
   const navigate = useNavigate();
   const { user, LogOut } = useContext(userContext)
 
+  let [isLight, setLight] = useState(true)
+
+
+  let LightMood = () => {
+    setLight(false)
+    console.log("dark")
+  }
+  let DarkMood = () => {
+    setLight(true)
+    console.log("light")
+  }
   const Logout = () => {
     LogOut().then(() => {
       navigate('/home')
@@ -25,6 +36,13 @@ const NavBar = () => {
 
             <li><Link to="">FAQ</Link></li>
             <li><Link to="/blog">Blog</Link></li>
+            {/* add light or dark mood toggle */}
+            {
+              isLight ?
+                <p onClick={LightMood}><FaToggleOff className='pointer h-[40px] w-[40px] m-16 lg:mx-0 my-2'></FaToggleOff></p>
+                :
+                <p onClick={DarkMood}><FaToggleOn className='pointer h-[40px] w-[40px] m-16 lg:mx-0 my-2'></FaToggleOn></p>
+            }
             {
               user?.displayName ? <button onClick={Logout} className="btn mr-3">LogOut</button> : ""
             }
@@ -32,19 +50,25 @@ const NavBar = () => {
 
           </ul>
         </div>
-       <div className='lg:flex'>
-       <FaBookReader className='h-[40px] w-[40px] mx-16 lg:mx-0'></FaBookReader>
-        <Link to="/home" className="btn btn-ghost normal-case text-xl">LEARN-ONLINE</Link>
-       </div>
+        <div className='lg:flex'>
+          <FaBookReader className='h-[40px] w-[40px] mx-16 lg:mx-0'></FaBookReader>
+          <Link to="/home" className="btn btn-ghost normal-case text-xl">LEARN-ONLINE</Link>
+        </div>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
           <li><Link to="/home">Courses</Link></li>
-
           <li><Link to="">FAQ</Link></li>
           <li><Link to="/blog">Blog</Link></li>
+          {/* add light or dark mood toggle */}
           {
-            user ? <button onClick={Logout} className="btn mr-3">LogOut</button> : ""
+            isLight ?
+              <p onClick={LightMood}><FaToggleOff className='pointer h-[40px] w-[40px] m-16 lg:mx-0 my-2'></FaToggleOff></p>
+              :
+              <p onClick={DarkMood}><FaToggleOn className='pointer h-[40px] w-[40px] m-16 lg:mx-0 my-2'></FaToggleOn></p>
+          }
+          {
+            user ? <button onClick={Logout} className="btn mx-3">LogOut</button> : ""
           }
         </ul>
       </div>
